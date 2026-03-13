787e71e7197d5d3177d5335cad2e90fe290f6c81
Asharib14 <asharib.harris@gmail.com>
2026-03-13 20:51:20 +0500

    acp

--- space invader part2.py
+++ space invader part2.py
@@ -3,9 +3,7 @@ import sys
 pygame.init()
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Space Invader")
-background = pygame.image.load("background.png")
-pygame.mixer.music.load("background.wav")
-pygame.mixer.music.play(-1)
+background = pygame.image.load("background1.jpg")
 running = True
 while running:
     screen.blit(background, (0,0))
